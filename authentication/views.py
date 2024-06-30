b67e774a1f9fc01fa2345f7c881a45793e8b00fa
cletusola <cletushunsu@gmail.com>
2024-06-30 21:05:16 +0100

    Refactored views and included changePassword view

--- authentication/views.py
+++ authentication/views.py
@@ -10,7 +10,7 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken 
 
 from .serializers import SignUpSerializer, SignInSerializer, ChangePasswordSerializer
-from .database import database 
+from fileshare.database import database 
 
 User = get_user_model()
 
@@ -41,14 +41,13 @@ class SignUpRequest(APIView):
 					"details": "Account created."
 				})
 
-			else:
-				# return error response 
-				return Response({
-					"status": "error",
-					"status_code": status.HTTP_400_BAD_REQUEST,
-					"details": serializer.errors,
-					"error_message": "Request failed. Invalid data format."
-				})
+			# return error response 
+			return Response({
+				"status": "error",
+				"status_code": status.HTTP_400_BAD_REQUEST,
+				"details": serializer.errors,
+				"error_message": "Request failed. Invalid data format."
+			})
 
 		except Exception as e:
 			logger.error(f"An error occurred in user signup request: {e}", exc_info=True)
@@ -70,13 +69,13 @@ class SignInRequest(APIView):
 
 	# handle post request for user signin 
 	async def post(self, request):
-		serializer = self.serializer_class(data.request.data)
+		serializer = self.serializer_class(data=request.data)
 
 		if serializer.is_valid(raise_exception=True):
 			loginId = serializer.validated_data["login_id"]
 			password = serializer.validated_data["password"]
 
-			user = await asyncio.to_thread(authenticate(request, username=loginId, password=password))
+			user = await asyncio.to_thread(authenticate, request, username=loginId, password=password)
 
 			if user is not None:
 				if user.is_active:
@@ -93,69 +92,70 @@ class SignInRequest(APIView):
 					})
 
 				# if user is not active 
-				else:
-					# return error response 
-					return Response({
-						"status": "error",
-						"status_code": status.HTTP_401_UNAUTHORIZED,
-						"details": "Account not active. Please contact our support team."
-					})
+				return Response({
+					"status": "error",
+					"status_code": status.HTTP_401_UNAUTHORIZED,
+					"details": "Account not active. Please contact our support team."
+				})
 
 			# if user is None (failed authentication)
-			else:
-				try:
-					# try to find user with the provided login id (username or email) 
-					user = await asyncio.to_thread(User.objects.get, (Q(username__iexact=loginId) | Q(email__iexact=loginId)))
-					if user:
-						# get user last failed login time
-						if user.last_failed_login:
-							time_since_last_failed_login = timezone.now() - user.last_failed_login
-							# if time_since_last_failed_login is > 24 (24 hours), reset user login_trials
-							if time_since_last_failed_login > timedelta(hours=24):
-								await asyncio.to_thread(user.reset_login_trials())
+			try:
+				# try to find user with the provided login id (username or email) 
+				user = await asyncio.to_thread(User.objects.get, (Q(username__iexact=loginId) | Q(email__iexact=loginId)))
+				if user:
+					# get user last failed login time
+					if user.last_failed_login:
+						time_since_last_failed_login = timezone.now() - user.last_failed_login
+						# if time_since_last_failed_login is > 24 (24 hours), reset user login_trials
+						if time_since_last_failed_login > timedelta(hours=24):
+							await asyncio.to_thread(user.reset_login_trials())
 
-						# if user hasn't exceeded the login trials 
-						if user.login_trials < user.max_login_trials:
-							await asyncio.to_thread(user.increment_login_trials()) # add 1 to the current login trial count
-							# return invalid username or password response 
-							return Response({
-								"status": "error",
-								"status_code": status.HTTP_401_UNAUTHORIZED,
-								"details": "Invalid login credentials."
-							})
-
-						# if login_trials is > max_login_trials
-						else:
-							return Response({
-								"status": "error",
-								"status_code": status.HTTP_401_UNAUTHORIZED,
-								"details": "You've tried too many times. Please try again after 24 hours"
-							})
-
-					# if no user is found 
-					else:
+					# if user hasn't exceeded the login trials 
+					if user.login_trials < user.max_login_trials:
+						await asyncio.to_thread(user.increment_login_trials()) # add 1 to the current login trial count
+						# return invalid username or password response 
 						return Response({
 							"status": "error",
 							"status_code": status.HTTP_401_UNAUTHORIZED,
 							"details": "Invalid login credentials."
 						})
 
-				except Exception as e:
-					logger.error(f"An error occured in user signin request: {e}", exc_info=True)
+					# if login_trials is > max_login_trials
 					return Response({
 						"status": "error",
-						"status_code": status.HTTP_500_INTERNAL_SERVER_ERROR,
-						"details": "An error occurred. Please try again later."
+						"status_code": status.HTTP_401_UNAUTHORIZED,
+						"details": "You've tried too many times. Please try again after 24 hours"
 					})
 
+				# if no user is found 
+				return Response({
+					"status": "error",
+					"status_code": status.HTTP_401_UNAUTHORIZED,
+					"details": "Invalid login credentials."
+				})
+
+			except User.DoesNotExist:
+                return Response({
+                    "status": "error",
+                    "status_code": status.HTTP_401_UNAUTHORIZED,
+                    "details": "Invalid login credentials."
+                })
+
+			except Exception as e:
+				logger.error(f"An error occured in user signin request: {e}", exc_info=True)
+				return Response({
+					"status": "error",
+					"status_code": status.HTTP_500_INTERNAL_SERVER_ERROR,
+					"details": "An error occurred. Please try again later."
+				})
+
 		# if serializer is not valid 
-		else:
-			return Response({
-				"status": "error",
-				"status_code": status.HTTP_400_BAD_REQUEST,
-				"details": serializer.errors,
-				"error_message": "Authentication request failed. Invalid data format or credentials."
-			})
+		return Response({
+			"status": "error",
+			"status_code": status.HTTP_400_BAD_REQUEST,
+			"details": serializer.errors,
+			"error_message": "Authentication request failed. Invalid data format or credentials."
+		})
 
 
 # signout view 
@@ -190,28 +190,27 @@ class SignOutRequest(APIView):
 					"details": "Logout successful."
 				})
 
-			else:
-				# get refresh token from form data if token is not provided in authentication header
-				refresh_token = request.data["refresh"]
+	
+			# get refresh token from form data if token is not provided in authentication header
+			refresh_token = request.data["refresh"]
 
-				if refresh_token:
-					# blacklist token 
-					token = RefreshToken(refresh_token)
-					token.blacklist()
-					
-					# return success response 
-					return Response({
-						"status": "success",
-						"status_code": status.HTTP_204_NO_CONTENT,
-						"details": "Logout successful"
-					})
+			if refresh_token:
+				# blacklist token 
+				token = RefreshToken(refresh_token)
+				token.blacklist()
+				
+				# return success response 
+				return Response({
+					"status": "success",
+					"status_code": status.HTTP_204_NO_CONTENT,
+					"details": "Logout successful"
+				})
 
-				else:
-					return Response({
-						"status": "error",
-						"status": status.HTTP_401_UNAUTHORIZED,
-						"details": "Unable to log you out. Please provide a valid token"
-					})
+			return Response({
+				"status": "error",
+				"status": status.HTTP_401_UNAUTHORIZED,
+				"details": "Unable to log you out. Please provide a valid token"
+			})
 
 		except Exception as e:
 			except Exception as e:
@@ -230,7 +229,7 @@ class ChangePasswordRequest(APIView):
 	serializer_class = ChangePasswordSerializer
 
 	async def post(self, request):
-		serializer = self.serializer_class(data=request.data)
+		serializer = self.serializer_class(data=request.data, context={'request': request})
 
 		if serializer.is_valid(raise_exception=True):
 			async with database.transaction():
@@ -242,13 +241,12 @@ class ChangePasswordRequest(APIView):
 				"details": "Password changed successfully."
 			})
 
-		else:
-			# return error response 
-			return Response({
-				"status": "error",
-				"status_code": status.HTTP_400_BAD_REQUEST,
-				"details": serializer.errors,
-				"error_message": "Request failed. Invalid data format."
-			})
+		# return error response 
+		return Response({
+			"status": "error",
+			"status_code": status.HTTP_400_BAD_REQUEST,
+			"details": serializer.errors,
+			"error_message": "Request failed. Invalid data format."
+		})
 
 
