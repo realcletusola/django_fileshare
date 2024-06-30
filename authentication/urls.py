3c518a4937fa2d116973226658a4435bcd24c9e8
cletusola <cletushunsu@gmail.com>
2024-06-30 21:05:39 +0100

    updated file

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path 
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenRefreshView
 
+from .views import (
+    SignUpRequest,SignInRequest,SignOutRequest
+    ChangePasswordRequest
+)
 
 urlpatterns = [
-
+    path('signup/', SignUpRequest.as_view(), name='signup'),
+    path('signin/', SignInRequest.as_view(), name='signin'),
+    path('signout/', SignOutRequest.as_view(), name='signout'),
+    path('change_password/', ChangePasswordRequest.as_view(), name='change_password'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 if settings.DEBUG:
