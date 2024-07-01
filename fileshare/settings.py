f05edc33637fd9a2eaacceea011c4dc77ae19bb2
cletusola <cletushunsu@gmail.com>
2024-07-01 10:00:57 +0100

    included users in installed apps. Remove the default sqlite3 database

--- fileshare/settings.py
+++ fileshare/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 
     # my apps
     'authentication', # authentication app
+    'users', # users app
 
     # libraries 
     'rest_framework', # django rest framework library 
@@ -73,18 +74,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'fileshare.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
